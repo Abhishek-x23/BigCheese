ce4f6d133f19931cf5bcb96313b6a0adc9661a39
Abhishek-x23 <aseth6779@gmail.com>
2025-12-27 00:05:25 +0530

    razorpay integration started

---
+++ actions/useractions.js
@@ -0,0 +1,28 @@
+"use server"
+
+import React from "react"
+import Razorpay from "razorpay"
+import Payment from "@/models/Payment"
+import User from "@/models/user"
+import connectDB from "@/components/connectDB"
+
+export const initiate = async (amount , to_username , paymentform) =>{
+
+    connectDB()
+    var instance = new Razorpay({ key_id: process.env.RAZORPAY_ID, key_secret: process.env.RAZORPAY_SECRET })
+
+    let options = {
+        amount : Number.parseInt(amount),
+        currency : "INR",
+    }
+
+    let x =  await instance.orders.create(options)
+
+    // creating a payment object which shows a pending payment in the database
+
+    await Payment.create({oid : x.id, amount : amount, to_username: to_username, name:paymentform.name, message : paymentform.message})
+
+    return x
+
+
+}
